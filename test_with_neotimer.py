f2b313d6373d3ac75bada30b78b91c3a0d1e0304
Jose Rullan <jerullan@yahoo.com>
2022-02-01 08:27:43 -0400

    Create test_with_neotimer.py

---
+++ test_with_neotimer.py
@@ -0,0 +1,64 @@
+from statemachine import *
+from neotimer import *
+
+counter = 0
+myTimer = Neotimer(1000)
+myMachine = StateMachine()
+
+#============================================================
+# States Logic Functions
+#============================================================
+def state1_logic():
+    global counter
+    if myMachine.execute_once:
+        
+        myTimer.start()    #<---- Start myTimer in state1
+        counter += 1
+        print("State 1 Logic Executed Once")
+   
+   
+def state2_logic():
+    global counter
+    if myMachine.execute_once:
+        
+        counter += 1
+        print("State 2 Logic Executed Once")
+    
+    
+def state3_logic():
+    global counter
+    if myMachine.execute_once:
+        
+        counter += 1
+        print("State 3 Logic Executed Once")
+
+#============================================================
+# State Transitions Functions
+#============================================================
+def state1_transition_to_2():
+    return True                                     #<---- Transition from state1 to state2 immediately
+
+def state2_transition_to_3():
+    return (True if myTimer.finished() else False)  #<---- Transition from state2 to state3 when myTimer finishes
+
+
+#============================================================
+# Add states to machine
+#============================================================
+state1 = myMachine.add_state(state1_logic)
+
+state2 = myMachine.add_state(state2_logic)
+
+state3 = myMachine.add_state(state3_logic)
+
+
+#============================================================
+# Add transitions to states
+#============================================================
+state1.add_transition(state1_transition_to_2, state2)
+
+state2.add_transition(state2_transition_to_3, state3) # lambda : True
+
+
+while counter < len(myMachine.state_list):
+    myMachine.run()
