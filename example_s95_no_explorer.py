1689fac939514bbdfcd1783ba971a651e811a67f
Jose Rullan <jerullan@yahoo.com>
2022-02-09 23:06:28 -0400

    Added example without explorer

--- example_s95_no_explorer.py
+++ example_s95_no_explorer.py
@@ -31,42 +31,27 @@ transition_timer = Neotimer(1000)
 led = Pin(25,Pin.OUT)
 led.off()
 
-# Pimoroni Explorer Initialization
-width = explorer.get_width()
-height = explorer.get_height()
-display_buffer = bytearray(width * height * 2)  # 2-bytes per pixel (RGB565)
-explorer.init(display_buffer)
-
-color_black = explorer.create_pen(0,0,0)
-color_white = explorer.create_pen(255,255,255)
-
-explorer.set_pen(color_black)
-explorer.clear()
-time.sleep(0.01) #<---- Needed otherwise crashes
-explorer.update()
+BUTTON_A = Pin(12,Pin.IN,Pin.PULL_UP)
+BUTTON_B = Pin(13,Pin.IN,Pin.PULL_UP)
+BUTTON_X = Pin(14,Pin.IN,Pin.PULL_UP)
+BUTTON_Y = Pin(15,Pin.IN,Pin.PULL_UP)
 
 
-print("After Pico Explorer",gc.mem_free())
+print("After Pico Explorer",gc.mem_free())    #<---- 115,392 bytes
 
-
-# Draw a string in the LCD
-def update_text(string):
-    #explorer.set_clip(5,10,150,15)
-    explorer.set_pen(color_black)
-    explorer.clear()
-    explorer.set_pen(color_white)
-    explorer.text(string,5,10,100)
 
 # Use to print messages to LCD and optionally (when DEBUG_MODE == True)
 # to the shell
 def notify(string):
     if DEBUG_MODE:
         print(string, gc.mem_free())
-    update_text(string)
     
 def blink():
     if blinker.repeat_execution():
         led.toggle()
+
+def is_pressed(pin):
+    return not pin.value()
 
 print("Before creating all logic and states",gc.mem_free())     #<---- 1952 bytes of logic and states
 
@@ -97,7 +82,7 @@ def execute_logic():
     
     led.on()
     
-    if debouncing_timer.debounce_signal(explorer.is_pressed(explorer.BUTTON_X)):
+    if debouncing_timer.debounce_signal(is_pressed(BUTTON_X)):
         state_machine.force_transition_to(suspending)
 
 def completing_logic():
@@ -143,7 +128,7 @@ def suspended_logic():
 
     led.off()
     
-    if debouncing_timer.debounce_signal(explorer.is_pressed(explorer.BUTTON_X)):
+    if debouncing_timer.debounce_signal(is_pressed(BUTTON_X)):
         state_machine.force_transition_to(unsuspending)
 
 def unsuspending_logic():
@@ -162,30 +147,30 @@ def held_logic():
     
     led.off()
 
-# def stopping_logic():
-#     if state_machine.execute_once:
-#         notify("Stopping")
-#         transition_timer.start()
-#  
-#     blink()
-#     
-#     if transition_timer.finished():
-#         state_machine.force_transition_to(stopped)
+def stopping_logic():
+    if state_machine.execute_once:
+        notify("Stopping")
+        transition_timer.start()
+ 
+    blink()
+    
+    if transition_timer.finished():
+        state_machine.force_transition_to(stopped)
 
 def stopped_logic():
     if state_machine.execute_once:
         notify("Stopped")
     
-    #led.off()
+    led.off()
     
-    if debouncing_timer.debounce_signal(explorer.is_pressed(explorer.BUTTON_A)):
+    if debouncing_timer.debounce_signal(is_pressed(BUTTON_A)):
         state_machine.force_transition_to(resetting)
 
-def aborted_logic():
-    if state_machine.execute_once:
-        notify("Aborted")
-
-    #led.off()
+# def aborted_logic():
+#     if state_machine.execute_once:
+#         notify("Aborted")
+# 
+#     led.off()
 
 print("After states logic",gc.mem_free())
 
@@ -202,22 +187,22 @@ suspending = state_machine.add_state(suspending_logic)
 suspended = state_machine.add_state(suspended_logic)
 unsuspending = state_machine.add_state(unsuspending_logic)
 held = state_machine.add_state(held_logic)
-# stopping = state_machine.add_state(stopping_logic)
+stopping = state_machine.add_state(stopping_logic)
 stopped = state_machine.add_state(stopped_logic)
-aborted = state_machine.add_state(aborted_logic)
+# aborted = state_machine.add_state(aborted_logic)
 
 
 #============================================================
 # State Transitions Functions (optional)
 #============================================================
 def next_transition():
-    if debouncing_timer.debounce_signal(explorer.is_pressed(explorer.BUTTON_A)) and transition_timer.finished():  #<---- Advance condition
+    if debouncing_timer.debounce_signal(is_pressed(BUTTON_A)) and transition_timer.finished():  #<---- Advance condition
         return True
     else:
         return False
 
 def stop_transition():
-    if debouncing_timer.debounce_signal(explorer.is_pressed(explorer.BUTTON_B)):  #<---- Stop condition
+    if debouncing_timer.debounce_signal(is_pressed(BUTTON_B)):  #<---- Stop condition
         return True
     else:
         return False
@@ -227,7 +212,7 @@ def stop_transition():
 #============================================================
 # Attach transitions to all states
 for state in state_machine.state_list:
-    state.attach_transition(stop_transition, stopped)
+    state.attach_transition(stop_transition, stopping)
 
 idle.attach_transition(next_transition,starting)
 execute.attach_transition(next_transition,completing)
@@ -235,15 +220,13 @@ completed.attach_transition(next_transition,resetting)
 
 print("After creating all logic and states",gc.mem_free())
 
-
+gc.collect()
 
 # Main Loop: Run the state machine here
 while True:
     
-    explorer.update()
-    
     # Determine if machine will run normally or in jog mode
-    if debouncing_timer.debounce_signal(explorer.is_pressed(explorer.BUTTON_Y)):
+    if debouncing_timer.debounce_signal(is_pressed(BUTTON_Y)):
         state_machine.jog_mode = True if state_machine.jog_mode == False else False
         if state_machine.jog_mode:
             print("Machine in Jog Mode")
@@ -251,10 +234,8 @@ while True:
             print("Machine in Run Mode")
 
     if state_machine.jog_mode:    
-        if debouncing_timer.debounce_signal(explorer.is_pressed(explorer.BUTTON_X)):
+        if debouncing_timer.debounce_signal(is_pressed(BUTTON_X)):
             state_machine.jog()
     
     # Run state machine state's logics
     state_machine.run()
-    
-    gc.collect()
